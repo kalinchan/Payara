31440e4a3d70b2c85aa06ab5809d058644d55063
David Matejcek <david.matejcek@payara.fish>
2020-04-16 19:14:19 +0200

    #4281 Removed commented code

--- appserver/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
+++ appserver/web/web-core/src/main/java/org/apache/catalina/session/StandardSession.java
@@ -723,15 +723,6 @@ public class StandardSession
             return true;
         }
 
-        /* SJSAS 6329289
-        if (maxInactiveInterval >= 0) { 
-            long timeNow = System.currentTimeMillis();
-            int timeIdle = (int) ((timeNow - thisAccessedTime) / 1000L);
-            if (timeIdle >= maxInactiveInterval) {
-                expire(true);
-            }
-        }
-        */
         // START SJSAS 6329289
         if (hasExpired()) {
             expire(true);
