858f3abdcd11a89cd53f276a98ae42367bd31e4e
luiseufrasio <luis.eufrasio@gmail.com>
2023-09-03 22:18:37 -0300

    FISH-7647 :  normalizing paths

--- appserver/web/web-naming/src/main/java/org/apache/naming/resources/FileDirContext.java
+++ appserver/web/web-naming/src/main/java/org/apache/naming/resources/FileDirContext.java
@@ -215,9 +215,9 @@ public class FileDirContext extends BaseDirContext {
             throw new IllegalArgumentException
                     (MessageFormat.format(rb.getString(LogFacade.FILE_RESOURCES_BASE), docBase));
         }
-        this.absoluteBase = base.getAbsolutePath();
+        this.absoluteBase = normalize(base.getAbsolutePath());
         try {
-            this.canonicalBase = base.getCanonicalPath();
+            this.canonicalBase = normalize(base.getCanonicalPath());
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
@@ -922,7 +922,7 @@ public class FileDirContext extends BaseDirContext {
             // Check that this file belongs to our root path
             String canPath = null;
             try {
-                canPath = file.toPath().toRealPath().toString();
+                canPath = normalize(file.toPath().toRealPath().toString());
             } catch (IOException e) {
             }
             if (canPath == null || !canPath.startsWith(canonicalBase)) {
